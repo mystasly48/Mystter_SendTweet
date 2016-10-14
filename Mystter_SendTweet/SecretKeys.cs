a97e40933ecc93b21e2a407bcb4c519411a3c373
Maisu <mystasly48@gmail.com>
2016-10-14 09:19:48 +0900

    Added: SecretKeys

---
+++ Mystter_SendTweet/SecretKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace Mystter_SendTweet {
+    public class SecretKeys {
+        public static readonly string ConsumerKey = "";
+        public static readonly string ConsumerSecret = "";
+    }
+}
