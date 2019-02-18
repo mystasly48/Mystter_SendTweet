7c64d4dfe1cdb9afff88a66e918d581b1b265ae5
Maisu <mystasly48@gmail.com>
2019-02-18 14:43:34 +0900

    Update Information
    忘れがちじゃね？

--- Mystter_SendTweet/Information.cs
+++ Mystter_SendTweet/Information.cs
@@ -2,7 +2,7 @@
     public class Information {
         public const string Title = "Mystter - Send Tweet";
         public const string TitleSimple = "Mystter";
-        public const string Version = "1.1.17";
+        public const string Version = "1.1.19";
         public const string Developer = "Mystasly";
         public const string Repository = "https://github.com/mystasly48/Mystter_SendTweet";
         public const string Twitter = "https://twitter.com/30msl";
