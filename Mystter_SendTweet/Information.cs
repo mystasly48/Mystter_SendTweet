95678fc42ccbf5d556fa356ec5a9d4466673cffe
Maisu <mystasly48@gmail.com>
2020-04-22 06:18:07 +0900

    Fix settings folder location

--- Mystter_SendTweet/Information.cs
+++ Mystter_SendTweet/Information.cs
@@ -8,7 +8,7 @@ namespace Mystter_SendTweet {
     public static readonly string Developer = "Mystasly";
     public static readonly string Repository = "https://github.com/mystasly48/Mystter_SendTweet";
     public static readonly string Twitter = "https://twitter.com/30msl";
-    public static readonly string SettingsFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+    public static readonly string SettingsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Title);
     public static readonly string SettingsFile = Path.Combine(SettingsFolder, "Settings.xml");
   }
 }
