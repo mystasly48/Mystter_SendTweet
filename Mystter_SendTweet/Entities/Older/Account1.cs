474bc3c7dbd376f1d378d48b2618388d01f2373d
Maisu <6381045+mystasly48@users.noreply.github.com>
2020-12-27 06:02:52 +0900

    Add older to newer settings parser

---
+++ Mystter_SendTweet/Entities/Older/Account1.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace Mystter_SendTweet.Entities.Older {
+  [XmlType("Account")]
+  public class Account1 {
+    public string AccessToken { get; set; }
+    public string AccessSecret { get; set; }
+    public string ScreenName { get; set; }
+    public long UserId { get; set; }
+
+    public Account1() { }
+  }
+}
